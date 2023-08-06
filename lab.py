27f96b4381c3f93fd43cef3a348ef5eb5f25e632
Basma <129796403+basiix1@users.noreply.github.com>
2023-08-06 14:35:48 +0300

    lab

---
+++ lab.py
@@ -0,0 +1,37 @@
+class BankAccount:
+    def __init__(self, account_holder, initial_balance):
+        self.account_holder = account_holder
+        self.initial_balance = initial_balance
+
+    def deposit(self, amount):
+        self.initial_balance += amount
+        return self.initial_balance
+    
+    def withdraw(self, amount):
+        if amount <= self.initial_balance:
+            self.initial_balance -= amount
+        else:
+            print("Insufficient funds. Withdrawal canceled.")
+        return self.initial_balance
+
+    def get_balance(self):   
+        return self.initial_balance
+
+    def get_account_holder(self):
+        return self.account_holder
+
+
+
+account = BankAccount("basma Al shaharani", 5000)
+
+print("Account holder:", account.get_account_holder())
+print("Current balance:", account.get_balance())
+
+account.deposit(0)
+print("Balance after deposit:", account.get_balance())
+
+account.withdraw(0)
+print("Balance after withdrawal:", account.get_balance())
+
+account.withdraw(0)  # This will print an error message
+print("Balance after withdrawal:", account.get_balance())
