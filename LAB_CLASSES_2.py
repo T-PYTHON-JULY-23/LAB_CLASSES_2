2b1d96767d713a8cd1f387cff300e5cf70cfb0db
sarahnaghi <sara.naghi@gmail.com>
2023-08-06 14:59:51 +0300

    lab classes 2 done

---
+++ LAB_CLASSES_2.py
@@ -0,0 +1,45 @@
+class BankAccount:
+
+    def __init__(self, account_holder:str , initial_balance:float)->None:
+        self.account_holder = account_holder 
+        self.initial_balance = initial_balance
+
+    def deposit(self, amount:float):
+       balance:float = self.initial_balance + amount
+       self.initial_balance= balance
+       return balance
+       
+    def withdraw(self, amount:float):
+         
+         if self.initial_balance > amount:
+          balance:float = self.initial_balance - amount
+          self.initial_balance= balance
+          return balance
+
+         else:
+            return( "There is insufficient balance!")
+             
+
+    def get_balance(self):
+        return self.initial_balance 
+
+    def get_account_holder(self):
+         return self.account_holder
+
+
+account1= BankAccount("Sarah",28375)
+
+print(account1.withdraw(700))
+print(account1.deposit(500))
+print(account1.get_balance())
+
+
+
+account2= BankAccount("Afnan",0)
+print(account2.deposit(500))
+print(account2.withdraw(700))
+print(account2.get_balance())
+
+
+print(f"Account holder: {account1.account_holder}, Balance: {account1.get_balance()}")
+print(f"Account holder: {account2.account_holder}, Balance: {account2.get_balance()}")
